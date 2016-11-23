b92e7da48d1590a595fa54ad8dc45b47d9615b5a
Daniel Medina <danielme_com@yahoo.com>
2016-11-23 19:52:38 +0100

    update

--- app/src/main/java/com/danielme/android/recyclerview/endless/MainActivity.java
+++ app/src/main/java/com/danielme/android/recyclerview/endless/MainActivity.java
@@ -2,6 +2,8 @@ package com.danielme.android.recyclerview.endless;
 
 import android.os.AsyncTask;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -9,8 +11,6 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.View;
 import android.widget.Toast;
-
-import com.danielme.android.recyclerview.endless.R;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -20,119 +20,126 @@ import java.util.List;
  */
 public class MainActivity extends AppCompatActivity {
 
-    private List<Item> colors;
-    private boolean hasMore;
-    private AsyncTask asyncTask;
-    private RecyclerView recyclerView;
+  private List<Item> colors;
+  private boolean hasMore;
+  private AsyncTask asyncTask;
+  private RecyclerView recyclerView;
 
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
-        setSupportActionBar(toolbar);
+  @Override
+  protected void onCreate(Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_main);
+    Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+    setSupportActionBar(toolbar);
 
-        colors = buildColors();
+    colors = buildColors();
 
-        hasMore = true;
+    hasMore = true;
 
-        recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
-        recyclerView.setAdapter(new MaterialPaletteAdapter(colors, new RecyclerViewOnItemClickListener() {
-            @Override
-            public void onClick(View v, int position) {
-                if (colors.get(position) instanceof Color) {
-                    Toast toast = Toast.makeText(MainActivity.this, String.valueOf(position), Toast.LENGTH_SHORT);
-                    int color = android.graphics.Color.parseColor(((Color) colors.get(position)).getHex());
-                    toast.getView().setBackgroundColor(color);
-                    toast.show();
-                }
-            }
-        }));
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.addItemDecoration(new DividerItemDecoration(this));
-        recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
-            @Override
-            public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
-                super.onScrolled(recyclerView, dx, dy);
-                if (hasMore && !(hasFooter())) {
-                    LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
-                    //position starts at 0
-                    if (layoutManager.findLastCompletelyVisibleItemPosition() == layoutManager.getItemCount() - 2) {
-
-                        asyncTask = new BackgroundTask();
-                        Void[] params = null;
-                        asyncTask.execute(params);
-                    }
-                }
-            }
-        });
-
-    }
-
-    @Override
-    protected void onPause() {
-        super.onPause();
-        if (asyncTask != null) {
-            asyncTask.cancel(false);
+    recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+    recyclerView.setAdapter(new MaterialPaletteAdapter(colors, new
+        RecyclerViewOnItemClickListener() {
+      @Override
+      public void onClick(View v, int position) {
+        if (colors.get(position) instanceof Color) {
+          Toast toast = Toast.makeText(MainActivity.this, String.valueOf(position), Toast
+              .LENGTH_SHORT);
+          int color = android.graphics.Color.parseColor(((Color) colors.get(position)).getHex());
+          toast.getView().setBackgroundColor(color);
+          toast.show();
         }
-    }
+      }
+    }));
+    recyclerView.setLayoutManager(new LinearLayoutManager(this));
+    recyclerView.addItemDecoration(new DividerItemDecoration(this));
+    recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
+      @Override
+      public void onScrolled(RecyclerView recyclerView, int dx, int dy) {
+        super.onScrolled(recyclerView, dx, dy);
+        if (hasMore && !(hasFooter())) {
+          LinearLayoutManager layoutManager = (LinearLayoutManager) recyclerView.getLayoutManager();
+          //position starts at 0
+          if (layoutManager.findLastCompletelyVisibleItemPosition()
+              == layoutManager.getItemCount() - 2) {
 
-    private class BackgroundTask extends AsyncTask<Void, Void, Void> {
-
-        @Override
-        protected void onPreExecute() {
+            //displays the footer after the onscroll listener
             colors.add(new Footer());
-            recyclerView.getAdapter().notifyItemInserted(colors.size() - 1);
+            Handler handler = new Handler();
+
+            final Runnable r = new Runnable() {
+              public void run() {
+                MainActivity.this.recyclerView.getAdapter().notifyItemInserted(colors.size() - 1);
+              }
+            };
+            handler.post(r);
+            asyncTask = new BackgroundTask();
+            asyncTask.execute((Object[]) null);
+          }
         }
+      }
+    });
 
-        @Override
-        protected Void doInBackground(Void... params) {
-            try {
-                Thread.sleep(3000);
-            } catch (InterruptedException e) {
-                Log.e(this.getClass().toString(), e.getMessage());
-            }
-            return null;
-        }
+  }
 
-        @Override
-        protected void onPostExecute(Void aVoid) {
-            int size = colors.size();
-            colors.remove(size - 1);//removes footer
-            colors.addAll(buildColors());
-            recyclerView.getAdapter().notifyItemRangeChanged(size - 1, colors.size() - size);
-        }
+  @Override
+  protected void onDestroy() {
+    super.onDestroy();
+    if (asyncTask != null) {
+      asyncTask.cancel(false);
+    }
+  }
 
+  private class BackgroundTask extends AsyncTask<Void, Void, Void> {
+
+    @Override
+    protected Void doInBackground(Void... params) {
+      try {
+        Thread.sleep(3000);
+      } catch (InterruptedException e) {
+        Log.e(this.getClass().toString(), e.getMessage());
+      }
+      return null;
     }
 
-    private boolean hasFooter() {
-        return colors.get(colors.size() - 1) instanceof Footer;
+    @Override
+    protected void onPostExecute(Void aVoid) {
+      int size = colors.size();
+      colors.remove(size - 1);//removes footer
+      colors.addAll(buildColors());
+      recyclerView.getAdapter().notifyItemRangeChanged(size - 1, colors.size() - size);
     }
 
-    private ArrayList<Item> buildColors() {
-        ArrayList<Item> colors = new ArrayList<Item>(13);
+  }
 
-        colors.add(new Color(getString(R.string.blue), getColorString(R.color.blue)));
-        colors.add(new Color(getString(R.string.indigo), getColorString(R.color.indigo)));
-        colors.add(new Color(getString(R.string.red), getColorString(R.color.red)));
-        colors.add(new Color(getString(R.string.green), getColorString(R.color.green)));
-        colors.add(new Color(getString(R.string.orange), getColorString(R.color.orange)));
-        colors.add(new Color(getString(R.string.grey), getColorString(R.color.bluegrey)));
-        colors.add(new Color(getString(R.string.amber), getColorString(R.color.teal)));
-        colors.add(new Color(getString(R.string.deeppurple), getColorString(R.color.deeppurple)));
-        colors.add(new Color(getString(R.string.bluegrey), getColorString(R.color.bluegrey)));
-        colors.add(new Color(getString(R.string.yellow), getColorString(R.color.yellow)));
-        colors.add(new Color(getString(R.string.cyan), getColorString(R.color.cyan)));
-        colors.add(new Color(getString(R.string.brown), getColorString(R.color.brown)));
-        colors.add(new Color(getString(R.string.teal), getColorString(R.color.teal)));
+  private boolean hasFooter() {
+    return colors.get(colors.size() - 1) instanceof Footer;
+  }
 
-        return colors;
-    }
+  private ArrayList<Item> buildColors() {
+    ArrayList<Item> colors = new ArrayList<>(13);
 
-    //returns the string hex value of a color in colors.xml
-    private String getColorString(int color) {
-        return "#" + Integer.toHexString(getResources().getColor(color)).toUpperCase().substring(2);
-    }
+    colors.add(new Color(getString(R.string.blue), getColorString(R.color.blue)));
+    colors.add(new Color(getString(R.string.indigo), getColorString(R.color.indigo)));
+    colors.add(new Color(getString(R.string.red), getColorString(R.color.red)));
+    colors.add(new Color(getString(R.string.green), getColorString(R.color.green)));
+    colors.add(new Color(getString(R.string.orange), getColorString(R.color.orange)));
+    colors.add(new Color(getString(R.string.grey), getColorString(R.color.bluegrey)));
+    colors.add(new Color(getString(R.string.amber), getColorString(R.color.teal)));
+    colors.add(new Color(getString(R.string.deeppurple), getColorString(R.color.deeppurple)));
+    colors.add(new Color(getString(R.string.bluegrey), getColorString(R.color.bluegrey)));
+    colors.add(new Color(getString(R.string.yellow), getColorString(R.color.yellow)));
+    colors.add(new Color(getString(R.string.cyan), getColorString(R.color.cyan)));
+    colors.add(new Color(getString(R.string.brown), getColorString(R.color.brown)));
+    colors.add(new Color(getString(R.string.teal), getColorString(R.color.teal)));
+
+    return colors;
+  }
+
+  //returns the string hex value of a color in colors.xml
+  private String getColorString(int colorId) {
+    return "#" + Integer.toHexString(ContextCompat.getColor(this, colorId)).toUpperCase()
+        .substring(2);
+  }
 
 }
--- app/src/main/java/com/danielme/android/recyclerview/endless/MaterialPaletteAdapter.java
+++ app/src/main/java/com/danielme/android/recyclerview/endless/MaterialPaletteAdapter.java
@@ -3,14 +3,11 @@ package com.danielme.android.recyclerview.endless;
 import android.graphics.drawable.GradientDrawable;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ProgressBar;
 import android.widget.TextView;
-
-import com.danielme.android.recyclerview.endless.R;
 
 import java.util.List;
 
@@ -18,111 +15,113 @@ import java.util.List;
  * @author danielme.com
  */
 public class MaterialPaletteAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    private List<Item> data;
+  private List<Item> data;
+  private RecyclerViewOnItemClickListener recyclerViewOnItemClickListener;
+
+  private static final int TYPE_COLOR = 0;
+  private static final int TYPE_FOOTER = 1;
+
+  public MaterialPaletteAdapter(@NonNull List<Item> data, RecyclerViewOnItemClickListener
+      recyclerViewOnItemClickListener) {
+    this.data = data;
+    this.recyclerViewOnItemClickListener = recyclerViewOnItemClickListener;
+  }
+
+  @Override
+  public int getItemViewType(int position) {
+    if (data.get(position) instanceof Color) {
+      return TYPE_COLOR;
+    } else if (data.get(position) instanceof Footer) {
+      return TYPE_FOOTER;
+    } else {
+      throw new RuntimeException("ItemViewType unknown");
+    }
+  }
+
+  @Override
+  public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    if (viewType == TYPE_COLOR) {
+      View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.row, parent, false);
+      return new PaletteViewHolder(row, recyclerViewOnItemClickListener);
+    } else {
+      View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.progress_footer,
+          parent, false);
+      return new FooterViewHolder(row);
+    }
+  }
+
+  @Override
+  public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+    if (holder instanceof PaletteViewHolder) {
+      Color color = (Color) data.get(position);
+
+      PaletteViewHolder paletteViewHolder = (PaletteViewHolder) holder;
+      paletteViewHolder.getTitleTextView().setText(color.getName());
+      paletteViewHolder.getSubtitleTextView().setText(color.getHex());
+
+      GradientDrawable gradientDrawable = (GradientDrawable) paletteViewHolder.getCircleView()
+          .getBackground();
+      int colorId = android.graphics.Color.parseColor(color.getHex());
+      gradientDrawable.setColor(colorId);
+    }
+    //FOOTER: nothing to do
+
+  }
+
+  @Override
+  public int getItemCount() {
+    return data.size();
+  }
+
+  private static class FooterViewHolder extends RecyclerView.ViewHolder {
+    public ProgressBar getProgressBar() {
+      return progressBar;
+    }
+
+    private ProgressBar progressBar;
+
+    public FooterViewHolder(View itemView) {
+      super(itemView);
+      progressBar = (ProgressBar) itemView.findViewById(R.id.footer);
+    }
+  }
+
+  private static class PaletteViewHolder extends RecyclerView.ViewHolder
+      implements View.OnClickListener {
+    private View circleView;
+    private TextView titleTextView;
+    private TextView subtitleTextView;
+
     private RecyclerViewOnItemClickListener recyclerViewOnItemClickListener;
 
-    private static final int TYPE_COLOR = 0;
-    private static final int TYPE_FOOTER = 1;
 
-    public MaterialPaletteAdapter(@NonNull List<Item> data, RecyclerViewOnItemClickListener recyclerViewOnItemClickListener) {
-        this.data = data;
-        this.recyclerViewOnItemClickListener = recyclerViewOnItemClickListener;
+    public PaletteViewHolder(View itemView, RecyclerViewOnItemClickListener
+        recyclerViewOnItemClickListener) {
+      super(itemView);
+      circleView = itemView.findViewById(R.id.circleView);
+      titleTextView = (TextView) itemView.findViewById(R.id.titleTextView);
+      subtitleTextView = (TextView) itemView.findViewById(R.id.subtitleTextView);
+      this.recyclerViewOnItemClickListener = recyclerViewOnItemClickListener;
+      itemView.setOnClickListener(this);
+    }
+
+    public TextView getTitleTextView() {
+      return titleTextView;
+    }
+
+    public TextView getSubtitleTextView() {
+      return subtitleTextView;
+    }
+
+    public View getCircleView() {
+      return circleView;
     }
 
     @Override
-    public int getItemViewType(int position) {
-        if (data.get(position) instanceof Color) {
-            return TYPE_COLOR;
-        } else if (data.get(position) instanceof Footer) {
-            return TYPE_FOOTER;
-        } else {
-            throw new RuntimeException("ItemViewType unknown");
-        }
+    public void onClick(View v) {
+      recyclerViewOnItemClickListener.onClick(v, getAdapterPosition());
     }
+  }
 
-    @Override
-    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
-        if (viewType == TYPE_COLOR) {
-            View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.row, parent, false);
-            PaletteViewHolder pvh = new PaletteViewHolder(row, recyclerViewOnItemClickListener);
-            return pvh;
-        } else {
-            View row = LayoutInflater.from(parent.getContext()).inflate(R.layout.progress_footer, parent, false);
-            FooterViewHolder vh = new FooterViewHolder(row);
-            return vh;
-        }
-    }
-
-    @Override
-    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
-        if (holder instanceof PaletteViewHolder) {
-            Color color = (Color) data.get(position);
-
-            PaletteViewHolder paletteViewHolder = (PaletteViewHolder) holder;
-            paletteViewHolder.getTitleTextView().setText(color.getName());
-            paletteViewHolder.getSubtitleTextView().setText(color.getHex());
-
-            GradientDrawable gradientDrawable = (GradientDrawable) paletteViewHolder.getCircleView().getBackground();
-            int colorId = android.graphics.Color.parseColor(color.getHex());
-            gradientDrawable.setColor(colorId);
-        }
-        //FOOTER: nothing to do
-
-    }
-
-    @Override
-    public int getItemCount() {
-        return data.size();
-    }
-
-
-    public static class PaletteViewHolder extends RecyclerView.ViewHolder
-            implements View.OnClickListener {
-        private View circleView;
-        private TextView titleTextView;
-        private TextView subtitleTextView;
-
-        private RecyclerViewOnItemClickListener recyclerViewOnItemClickListener;
-
-
-        public PaletteViewHolder(View itemView, RecyclerViewOnItemClickListener recyclerViewOnItemClickListener) {
-            super(itemView);
-            circleView = itemView.findViewById(R.id.circleView);
-            titleTextView = (TextView) itemView.findViewById(R.id.titleTextView);
-            subtitleTextView = (TextView) itemView.findViewById(R.id.subtitleTextView);
-            this.recyclerViewOnItemClickListener = recyclerViewOnItemClickListener;
-            itemView.setOnClickListener(this);
-        }
-
-        public TextView getTitleTextView() {
-            return titleTextView;
-        }
-
-        public TextView getSubtitleTextView() {
-            return subtitleTextView;
-        }
-
-        public View getCircleView() {
-            return circleView;
-        }
-
-        @Override
-        public void onClick(View v) {
-            recyclerViewOnItemClickListener.onClick(v, getAdapterPosition());
-        }
-    }
-
-    public static class FooterViewHolder extends RecyclerView.ViewHolder {
-        public ProgressBar getProgressBar() {
-            return progressBar;
-        }
-
-        private ProgressBar progressBar;
-
-        public FooterViewHolder(View itemView) {
-            super(itemView);
-            progressBar = (ProgressBar) itemView.findViewById(R.id.footer);
-        }
-    }
 
 }
